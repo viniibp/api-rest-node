b941ef9cbb6a0fb006007d1d05a2be28dc1860be
viniibp <viniibp@gmail.com>
2024-05-16 10:18:38 -0300

    setup server with knex and sqlite. routes to test

---
+++ src/database.ts
@@ -0,0 +1,16 @@
+import setupKnex, { type Knex } from "knex";
+
+export const config: Knex.Config = {
+  client: "sqlite",
+  connection: {
+    filename: "./db/app.db",
+  },
+  useNullAsDefault: true,
+  migrations: {
+    extension: "ts",
+    directory: "./db/migrations/",
+  },
+};
+
+export const knex = setupKnex(config);
+
