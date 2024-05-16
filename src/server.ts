9c2ffdde3fad6643a2d0d1325207355a8558a022
viniibp <viniibp@gmail.com>
2024-05-16 11:33:36 -0300

    migrations and post/get transactions

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,33 @@
 import fastify from "fastify";
+import crypto from "node:crypto";
 import { knex } from "./database";
 
 const app = fastify();
 
 app.get("/hello", async () => {
-  const tables = await knex("sqlite_schema").select("*");
-  return tables;
+  return "Hello World!";
+});
+
+app.get("/schema", async () => {
+  const schema = await knex("sqlite_schema").select("*");
+  return schema;
+});
+
+app.get("/transactions", async () => {
+  const transaction = await knex("transactions").select("*");
+  return transaction;
+});
+
+app.post("/transactions", async () => {
+  const transaction = await knex("transactions")
+    .insert({
+      id: crypto.randomUUID(),
+      title: "Transação de teste",
+      amount: 1000,
+    })
+    .returning("*");
+
+  return transaction;
 });
 
 app.listen({ port: 3333 }).then(() => console.log("HTTP Server Running!"));
