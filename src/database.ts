c87f6eddc3dff8daa203879b8fa043417ca1c94b
viniibp <viniibp@gmail.com>
2024-05-16 11:43:38 -0300

    using .env

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,14 @@
+import "dotenv/config";
 import setupKnex, { type Knex } from "knex";
+
+if(!process.env.DATABASE_URL){
+  throw new Error('DATABASE_URL env not found.');
+}
 
 export const config: Knex.Config = {
   client: "sqlite",
   connection: {
-    filename: "./db/app.db",
+    filename: process.env.DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
@@ -13,4 +18,3 @@ export const config: Knex.Config = {
 };
 
 export const knex = setupKnex(config);
-
